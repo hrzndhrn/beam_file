67f84663fbbd6d40aea2a8c86e2098655c2a197c
Marcus Kruse <esurk.sucram@gmail.com>
2021-03-28 15:04:19 +0200

    Remove example

--- test/fixtures/default.ex
+++ test/fixtures/default.ex
@@ -3,11 +3,3 @@ defmodule Default do
 
   def gogo(a, x \\ 666_000, y \\ 1_000), do: a + x + y
 end
-
-defmodule Say do
-  def hello(name \\ "World"), do: name |> format() |> puts()
-
-  def format(name), do: "Hello, #{name}!"
-
-  defdelegate puts(item), to: IO
-end
