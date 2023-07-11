7a38113875996a798e7750db07fdd66799f3846f
Marcus Kruse <esurk.sucram@gmail.com>
2023-07-11 22:13:45 +0200

    Update tests

--- mix.exs
+++ mix.exs
@@ -57,10 +57,10 @@ defmodule BeamFile.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.14", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.30", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.16", only: :test},
       {:recode, "~> 0.5.1", only: [:dev, :test]}
     ]
   end
--- test/fixtures/1.13.4/comps.exs
+++ test/fixtures/1.13.4/comps.exs
@@ -40,6 +40,61 @@ defmodule Elixir.Comps do
         do: (acc -> Map.update(acc, <<x>>, 1, fn x1 -> :erlang.+(x1, 1) end))
   end
 
+  def seven do
+    :erlang.++(
+      for x <- [1, 2] do
+        x
+      end,
+      for y <- [3, 4] do
+        y
+      end
+    )
+  end
+
+  def eight do
+    :erlang.--(
+      for x <- [1, 2] do
+        x
+      end,
+      for y <- [3, 4] do
+        y
+      end
+    )
+  end
+
+  def nine(list) do
+    :erlang.++(
+      list,
+      Enum.sort(
+        for x <- [1, 2] do
+          x
+        end
+      )
+    )
+  end
+
+  def ten(list) do
+    :erlang.++(
+      list,
+      Enum.sort(
+        for x <- [1, 2] do
+          x
+        end
+      )
+    )
+  end
+
+  def eleven(list) do
+    :erlang.--(
+      list,
+      Enum.sort(
+        for x <- [1, 2] do
+          x
+        end
+      )
+    )
+  end
+
   def users(users) do
     for {type, name} when :erlang."/="(type, :guest) <- users do
       String.upcase(name)
