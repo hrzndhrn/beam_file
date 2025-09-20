8aaaab86e3922d0af2b5121986402650ce5b1671
Marcus Kruse <esurk.sucram@gmail.com>
2025-09-20 14:43:34 +0200

    Update fixtures

--- test/fixtures/1.14.5/def_for_ast.exs
+++ test/fixtures/1.14.5/def_for_ast.exs
@@ -5,55 +5,51 @@
      do:
        {:__block__, [],
         [
-          {:def, [line: 2, column: 7], [{:for, [], Elixir}, [do: :for]]},
-          {:def, [line: 6, column: 7],
+          {:def, [line: 2], [{:for, [], Elixir}, [do: :for]]},
+          {:def, [line: 6],
            [
-             {:for, [], [{:x, [version: 0, line: 6, column: 11], nil}]},
+             {:for, [], [{:x, [version: 0, line: 6], nil}]},
              [
-               do: {:{}, [line: 7, column: 5], [{:x, [version: 0, line: 7, column: 6], nil}]}
+               do: {:{}, [line: 7], [{:x, [version: 0, line: 7], nil}]}
              ]
            ]},
-          {:def, [line: 10, column: 7],
+          {:def, [line: 10],
            [
              {:for, [],
               [
-                {:x, [version: 0, line: 10, column: 11], nil},
-                {:y, [version: 1, line: 10, column: 14], nil}
+                {:x, [version: 0, line: 10], nil},
+                {:y, [version: 1, line: 10], nil}
               ]},
              [
-               do:
-                 {{:x, [version: 0, line: 11, column: 6], nil},
-                  {:y, [version: 1, line: 11, column: 9], nil}}
+               do: {{:x, [version: 0, line: 11], nil}, {:y, [version: 1, line: 11], nil}}
              ]
            ]},
-          {:def, [line: 14, column: 7],
+          {:def, [line: 14],
            [
              {:foo, [],
               [
-                {:x, [version: 0, line: 14, column: 11], nil},
-                {:y, [version: 1, line: 14, column: 14], nil}
+                {:x, [version: 0, line: 14], nil},
+                {:y, [version: 1, line: 14], nil}
               ]},
              [
-               do:
-                 {{:x, [version: 0, line: 15, column: 6], nil},
-                  {:y, [version: 1, line: 15, column: 9], nil}}
+               do: {{:x, [version: 0, line: 15], nil}, {:y, [version: 1, line: 15], nil}}
              ]
            ]},
-          {:def, [line: 18, column: 7],
+          {:def, [line: 18],
            [
              {:for, [],
               [
-                {:x, [version: 0, line: 18, column: 11], nil},
-                {:y, [version: 1, line: 18, column: 14], nil},
-                {:z, [version: 2, line: 18, column: 17], nil}
+                {:x, [version: 0, line: 18], nil},
+                {:y, [version: 1, line: 18], nil},
+                {:z, [version: 2, line: 18], nil}
               ]},
              [
                do:
-                 {:{}, [line: 18, column: 25],
+                 {:{}, [line: 18],
                   [
-                    {:x, [version: 0, line: 18, column: 26], nil},
-                    {:y, [version: 1, line: 18, column: 29], nil},
-                    {:z, [version: 2, line: 18, column: 32], nil}
+                    {:x, [version: 0, line: 18], nil},
+                    {:y, [version: 1, line: 18], nil},
+                    {:z, [version: 2, line: 18], nil}
                   ]}
              ]
            ]}
